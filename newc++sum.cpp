81b3f8986627862f62d9c7f6dd4573cff8369e2f
Manmohan Joshi <mannuj039@gmail.com>
2024-09-07 23:14:11 +0530

    Add files via upload

---
+++ newc++sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num1,num2;
+    cout<<"Enter the vale of num1:\n"; // <<insertion operator
+    cin>>num1; // >> extraction operator
+
+    cout<<"Enter the vale of num2:\n"; // <<insertion operator
+    cin>>num2; // >> extraction operator
+
+    cout<<"The sum is:"<<num1+num2;
+
+    return 0;
+
+}
